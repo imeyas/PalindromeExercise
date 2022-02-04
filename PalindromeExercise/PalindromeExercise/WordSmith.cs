660a5cc6b93de863b9898e8dd14e02618e70b39d
Ebo Onwunali <ebo@njgeneral.com>
2022-02-03 19:53:03 -0500

    Finished code for test build out and method to pass tests

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsAPalindrome(string word)
+        {
+            var reverse = "";
+
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word.ToLower()[i];
+            }
+
+            if (reverse == word.ToLower())
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,45 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("redivider", true)]
+        [InlineData("civic", true)]
+        [InlineData("radar", true)]
+        [InlineData("eric", false)]
+        [InlineData("rotor", true)]
+        [InlineData("kayak", true)]
+        [InlineData("gold", false)]
+        [InlineData("Gold", false)]
+        [InlineData("reviver", true)]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("Golf", false)]
+        [InlineData("golf", false)]
+        [InlineData("madam", true)]
+        [InlineData("Madam", true)]
+        [InlineData("refer", true)]
+        [InlineData("Refer", true)]
+        [InlineData("Redivider", true)]
+        [InlineData("Civic", true)]
+        [InlineData("Radar", true)]
+        [InlineData("Eric", false)]
+        [InlineData("Rotor", true)]
+        [InlineData("Kayak", true)]
+        [InlineData("Reviver", true)]
+
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+
+            var tester = new WordSmith();
+
+            //Act
+
+            var actual = WordSmith.IsAPalindrome(word);
+
+            //Assert
+
+            Assert.Equal(expected, actual);
 
         }
     }
